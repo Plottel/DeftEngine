c4f47781bdbb6355a9b89530bdf5ba63bb86ea7e
Plottel <matt.noone95@gmail.com>
2017-12-11 15:51:29 +1100

    QUALITY: Overhaul folder structure into modules containing relevant Components/Systems/Actions/Events/Utility Classes

---
+++ DeftEngine/Physics/PhysicsSystems/System_VelocityMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeftEngine
+{
+    public class System_Process_VelocityMovement : ISystem, IQuerySystem, IProcessSystem
+    {
+        public bool Query(Entity e)
+            => e.Has<Component_Spatial>() && e.Has<Component_Velocity>();
+
+        public void Process(ECSData ecsData)
+        {
+            var entities = ecsData.pool.Query(this);
+            Component_Velocity velComp;
+
+            foreach (var e in entities)
+            {
+                velComp = e.Get<Component_Velocity>();
+                e.Get<Component_Spatial>().pos += velComp.velocity * velComp.speed;
+            }                
+        }
+    }
+}
